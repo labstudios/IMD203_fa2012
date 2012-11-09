1aff993cdf277acbcc491d4f55106485fbb957ef
Brent <labstudios@gmail.com>
2012-11-09 03:47:56 +0000

    week 6 complete

---
+++ week_6/process.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+
+	<title>Thanks!</title>
+</head>
+
+<body>
+
+<div>
+    <h3>Thanks for your order!</h3>
+    <p>You will probably recieve it after you are dead.</p>
+</div>
+
+</body>
+</html>
